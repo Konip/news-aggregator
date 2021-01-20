675cd58ffd0c6015001793c214ea38b020e12720
Konip <gunpowderbit@gmail.com>
2021-01-20 22:26:32 +0300

    first com

---
+++ src/copmponents/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Post.css"
+export default function Post() {
+    return (
+        <div className='post'>
+            <div className="post__image"></div>
+            <div className="post__info"></div>
+            <h2 className="post__title">lalalala</h2>
+            <p className="post__description">ololol</p>
+        </div>
+    )
+}
