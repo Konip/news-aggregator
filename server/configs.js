01340f7dfffa40175690ec4a89197de68759b3da
Konip <gunpowderbit@gmail.com>
2021-01-22 18:04:24 +0300

    fix

---
+++ server/configs.js
@@ -0,0 +1,22 @@
+const elems = {
+    riaNews: {
+        title: '.article__header h1',
+        image: '.photoview__open img',
+        text: '.article__text',
+    },
+    rgNews: {
+        title: '.b-material-head__row h1',
+        image: '.b-material-img__img',
+        text: '.b-material-wrapper__text p',
+    },
+    tassNews: {
+        title: '.news-header__title',
+        image: '.text-include-photo__img',
+        text: '.text-block p',
+    },
+}
+
+export default elems
+// module.exports = {
+//     elems: elems,
+// }
---
+++ server/parserPost.js
@@ -0,0 +1,28 @@
+// const unirest = require("unirest")
+// const cheerio = require("cheerio")
+import unirest from "unirest"
+import cheerio from "cheerio"
+
+ const parserPost = (url, elems) => {
+    unirest
+        .get(url)
+        .end(response => {
+
+            const body = response.body
+            const $ = cheerio.load(body)
+            const title = $(elems.title).text().trim()
+            const image = $(elems.image).attr('src')
+            const text = $(elems.text).text().trim()
+
+            const post = {
+                title: title,
+                image: image,
+                text: text,
+            }
+            console.log(post)
+        })
+
+
+}
+// export default parserPost
+module.exports = parserPost
